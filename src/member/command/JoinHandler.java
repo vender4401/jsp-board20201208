362503db9fc799208e81ce995a2487fe603d66c3
YoonKwan <YoonKwan@DESKTOP-6JVEAL1>
2020-12-08 17:52:21 +0900

    1208

---
+++ src/member/command/JoinHandler.java
@@ -0,0 +1,14 @@
+package member.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mvc.command.CommandHandler;
+
+public class JoinHandler implements CommandHandler {
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// TODO Auto-generated method stub
+		return "joinForm";
+	}
+}
